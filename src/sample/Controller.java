0f4a04ff8a73bb6c7c626eabc9545afa462915ab
JulienCarrau <juliencarrau@orange.fr>
2020-05-29 10:03:04 +0200

    Controller implementation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Canvas drawing;
+
+    @FXML
+    private RadioButton selectMoveRadio, ellipseRadio, rectangleRadio, lineRadio;
+
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private Button deleteButton, cloneButton;
+
+    @FXML
+    private ToggleGroup group; // unused
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ellipseRadio.fire(); // Select ellipse per default
+
+
+    }
 }
