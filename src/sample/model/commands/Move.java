06710830f55c10d03d13d4f0e3e6cad57711370b
JulienCarrau <juliencarrau@orange.fr>
2020-05-29 17:13:54 +0200

    change name method

---
+++ src/sample/model/commands/Move.java
@@ -0,0 +1,19 @@
+package sample.model.commands;
+
+public class Move implements ICommand {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+
+    @Override
+    public void redo() {
+
+    }
+}
